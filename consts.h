4a850985d47f8a06087c20cdad3d906e2908b0f9
Фетисов Данила <fetisoff.danila2014@yandex.ru>
2021-12-20 14:08:58 +0300

    Constants

---
+++ consts.h
@@ -0,0 +1,13 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#define STRING0 "UNDEFINED"
+#define NUMBER0 0
+#define PRICE0 0.0f
+#define SIZE0 10
+#define CRIT_FULL 0.7
+
+#define SHOW_MEM_USAGE 0
+#define SHOW_GUI 1
+
+#endif // CONSTS_H
